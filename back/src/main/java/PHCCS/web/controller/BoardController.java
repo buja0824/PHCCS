7a7494e834059e18886e3c536156ffc8c6a53dca
WooXher <127946067+WooXher@users.noreply.github.com>
2024-08-24 18:01:50 +0900

    업로드 파일 이름, 경로 만들어야함

--- back/src/main/java/PHCCS/web/controller/BoardController.java
+++ back/src/main/java/PHCCS/web/controller/BoardController.java
@@ -3,13 +3,19 @@ package PHCCS.web.controller;
 import PHCCS.SessionConst;
 import PHCCS.domain.Member;
 import PHCCS.domain.Post;
+import PHCCS.domain.UploadFile;
+import PHCCS.file.FileStore;
 import PHCCS.web.repository.domain.PostModifyParam;
 import PHCCS.web.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,17 +24,34 @@ import org.springframework.web.bind.annotation.*;
 public class BoardController {
 
     private final PostService service;
-
+    private final FileStore fileStore;
     @PostMapping("/post")
     public ResponseEntity<?> createPost(
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
-            @RequestBody Post post){
+            @RequestBody PostDto dto) throws IOException {
 
         log.info("createPost()");
 //        if(!isLogin(loginMember)){
 //            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인하지 않은 사용자는 접근할 수 없습니다.");
 //        }
         //TODO
+        List<UploadFile> storeImgs = fileStore.storeFiles(dto.getImageFiles(),
+                loginMember.getId(),
+                dto.getTitle(),
+                dto.getCategory());
+        List<UploadFile> storeVids = fileStore.storeFiles(dto.getImageFiles(),
+                loginMember.getId(),
+                dto.getTitle(),
+                dto.getCategory());
+
+        Post post = new Post();
+        post.setCategory(dto.getCategory());
+        post.setTitle(dto.getTitle());
+        post.setContent(dto.getContent());
+        post.setAuthor(dto.getAuthor());
+        post.setWriteTime(dto.getWriteTime());
+        post.setImageFiles(storeImgs);
+        post.setVideoFiles(storeVids);
         ResponseEntity<?> save = service.save(/*loginMember.getId()*/2L, post);
 
         return save;
--- back/src/main/java/PHCCS/web/service/PostService.java
+++ back/src/main/java/PHCCS/web/service/PostService.java
@@ -15,20 +15,20 @@ import org.springframework.stereotype.Service;
 public class PostService {
     private final PostRepository repository;
 
-    public ResponseEntity<?> save(Long id, Post post){
+    public ResponseEntity<?> save(Long memberId, Post post){
         switch (post.getCategory()){
             case "community_board":
-                if(repository.communitySave(id, post) <= 0){
+                if(repository.communitySave(memberId, post) <= 0){
                     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("내부 오류 발생 게시글 등록 실패");
                 }
                 break;
             case "qna_board":
-                if(repository.qnaSave(id, post) <= 0){
+                if(repository.qnaSave(memberId, post) <= 0){
                     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("내부 오류 발생 게시글 등록 실패");
                 }
                 break;
             case "vet_board":
-                if(repository.vetSave(id, post) <= 0){
+                if(repository.vetSave(memberId, post) <= 0){
                     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("내부 오류 발생 게시글 등록 실패");
                 }
                 break;
