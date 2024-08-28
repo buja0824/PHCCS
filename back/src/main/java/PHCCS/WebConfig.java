d2eb943a0435e5ebc5964ed2c8648eb42e912399
jingujeon <wlsrn158@gmail.com>
2024-08-28 22:29:46 +0900

    인터셉트 구현
    구현 중
    현재 이해없이 코드만 넣은 상태
    추후 코드 이해후 수정이 필요함

---
+++ back/src/main/java/PHCCS/WebConfig.java
@@ -0,0 +1,25 @@
+package PHCCS;
+
+import PHCCS.web.interceptor.LogInterceptor;
+import PHCCS.web.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/", "/members/add", "/auth/signin", "/auth/logout",
+                        "/css/**", "/*.ico", "/error"
+                );
+    }
+}
