8260a6aabf07e3942872804fcd30f582ecd6963e
jingujeon <wlsrn158@gmail.com>
2024-10-03 13:57:03 +0900

    Update WebConfig.java

--- back/src/main/java/PHCCS/WebConfig.java
+++ back/src/main/java/PHCCS/WebConfig.java
@@ -21,11 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
     private final JwtUtil jwtUtil;
 
     // CORS 설정
+    // (현재 프론트, 백엔드 포트를 3030로 통일 중이여서 cops 설정이 필요 없지만 백엔드 포트를 다르게 해도 프로그램이 동작될 수 있도록 cors 설정을 미리 만들어 놓음)
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3030", "http://localhost:8081")  // 허용할 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
+                .allowedOrigins("http://localhost:3030")  // 허용할 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")  // 허용할 HTTP 메서드(추가 가능)
                 .allowedHeaders("Authorization", "Content-Type")  // 허용할 헤더
                 .allowCredentials(true);  // 인증 관련 헤더 허용 (예: 쿠키)
     }
