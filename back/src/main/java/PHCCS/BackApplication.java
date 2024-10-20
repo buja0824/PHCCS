3b5a61c446ceae6b9a019f1d323dee2dbe29e8e2
Woocheol <127946067+WooXher@users.noreply.github.com>
2024-10-20 20:55:32 +0900

    어드민화면 - 임시

--- back/src/main/java/PHCCS/BackApplication.java
+++ back/src/main/java/PHCCS/BackApplication.java
@@ -1,5 +1,6 @@
 package PHCCS;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
